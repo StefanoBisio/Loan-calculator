5b9b734996db17e468487af9e3d5ba21442a8c38
Stefano Bisio <s.b.milano@gmail.com>
2017-07-25 14:21:13 +0100

    Added comments

--- main.js
+++ main.js
@@ -1,10 +1,13 @@
 document.getElementById("calculate").onclick = function(){
     
+    /* on click get the values of the text fields*/
+    /* make the interest a percentage */
     var P = document.getElementById("principle").value;
     var i = document.getElementById("interest").value/100;
     var t = document.getElementById("time").value;
-
+    
     document.getElementById("resulth1").innerHTML = "The total amount due will be " + (P*(1+(i/12))**(t*12)).toFixed(2);
+    /* Preventdefault() to avoid result to be refreshed and cancelled */
     event.preventDefault();
     
     document.getElementById("resulth3").innerHTML = "Monthly interest due: " + (P*(1+(i/12))-P).toFixed(2);
